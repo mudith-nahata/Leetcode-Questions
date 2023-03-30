614f97ba5c97d791677a67bee94054b58e2c5dc9
Mudith Nahata <96544398+mudith-nahata@users.noreply.github.com>
2023-03-30 17:22:03 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Largest Element in Array/largest-element-in-array.py
@@ -0,0 +1,36 @@
+#User function Template for python3
+
+def largest( arr, n):
+    temp=arr[0]
+    for i in range(0,n):
+        if arr[i]>temp:
+            temp=arr[i]
+    return temp
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
+        print(largest(a, n))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+    
+# } Driver Code Ends
