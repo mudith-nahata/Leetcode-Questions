5b0335c71612625e4ce29789f1cb17774c624a00
Mudith Nahata <96544398+mudith-nahata@users.noreply.github.com>
2023-04-03 22:19:58 +0530

    Updated Solution - GfG to GitHub

--- Medium/Majority Element/majority-element.py
+++ Medium/Majority Element/majority-element.py
@@ -1,24 +1,16 @@
 class Solution:
     def majorityElement(self, a, n):
         #Your code here
-        c=0
-        res=0
-        for i in range(n):
-            if c==0:
-                res=a[i]
-            if a[i]==res:
-                c+=1
+        d={}
+        for num in a:
+            if num in d:
+                d[num]+=1
             else:
-                c-=1
-        c=0
-        for i in a:
-            if i == res:
-                c+=1
-        if c>n//2:
-            return res
-        else:
-            return -1
-        
+                d[num]=1
+        for num in d:
+            if d[num]>len(a)//2:
+                return num
+        return -1
 
                     
                 
