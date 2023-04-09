273b98534a2e42d0ed0bd802cff4beec69221b04
Mudith Nahata <96544398+mudith-nahata@users.noreply.github.com>
2023-04-09 23:21:54 +0530

    Updated Solution - GfG to GitHub

--- Easy/Rotate by 90 degree/rotate-by-90-degree.py
+++ Easy/Rotate by 90 degree/rotate-by-90-degree.py
@@ -5,13 +5,25 @@ class Solution:
     
     #Function to rotate matrix anticlockwise by 90 degrees.
     def rotateby90(self,matrix, n):
+              #reverse rows
+        p1=0
+        p2=n-1
+        while p1<p2:
+            for i in range(n):
+                matrix[i][p1],matrix[i][p2]=matrix[i][p2],matrix[i][p1]
+            p1+=1
+            p2-=1
         for i in range(n):
             for j in range(i):
                 matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
-        for i in range(n//2):
-            for j in range(n):
-                matrix[i][j],matrix[n-1-i][j]=matrix[n-1-i][j],matrix[i][j]
-
+        #reverse rows
+        #p1=0
+        #p2=n-1
+        #while p1<p2:
+            #for i in range(n):
+                #matrix[i][p1],matrix[i][p2]=matrix[i][p2],matrix[i][p1]
+            #p1+=1
+            #p2-=1
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
