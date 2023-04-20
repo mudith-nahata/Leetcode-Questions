bf8532ece5988d4aa0b6c7ed04d147b245421f64
Mudith Nahata <96544398+mudith-nahata@users.noreply.github.com>
2023-04-20 23:09:52 +0530

    Update binary-search.py

--- Basic/Binary Search/binary-search.py
+++ Basic/Binary Search/binary-search.py
@@ -2,9 +2,18 @@
 
 class Solution:	
 	def binarysearch(self, arr, n, k):
-	    for i in range(n):
-	        if arr[i]==k:
-	            return i
+	    if arr!=sorted(arr):
+	        arr=sorted(arr)
+	    low=0
+	    high=n-1
+	    while low<=high:
+	        mid=low+(high-low)//2
+	        if arr[mid]==k:
+	            return mid
+	        elif arr[mid]>k:
+	            high=mid-1
+	        else:
+	            low=mid+1
 	    return -1
 	       
 
