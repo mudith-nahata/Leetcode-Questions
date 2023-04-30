ceb6212c312dbc746da33463ef8a75f9cdd7e15c
Mudith Nahata <96544398+mudith-nahata@users.noreply.github.com>
2023-04-30 18:43:49 +0530

    Updated Solution - GfG to GitHub

--- Easy/Rotation/rotation.py
+++ Easy/Rotation/rotation.py
@@ -1,21 +1,13 @@
 #User function Template for python3
 class Solution:
     def findKRotation(self,arr,  n):
-        low=0
-        high=n-1
-        while(low <= high):
-            mid=low + ((high - low)// 2)
-            prev=(mid - 1 + n) % n
-            next = (mid + 1) % n
-            if(arr[mid]<=arr[prev]
-               and arr[mid]<=arr[next]):
-                return mid
-            elif (arr[mid]<=arr[high]):
-                high=mid- 1
-            elif (arr[mid]>=arr[low]):
-                low=mid+1
-        return 0
-
+        ans=0
+        for i in range(n-1):
+            if arr[i]>arr[i+1]:
+                ans=i+1
+            else:
+                continue
+        return ans
 
 #{ 
  # Driver Code Starts
